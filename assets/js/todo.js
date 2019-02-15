ed946e042a09d732138ab6c7ef80093909a7fb63
Louie F <fitz4492@gmail.com>
2019-02-15 03:32:22 -0600

    basic html structure and js functions

---
+++ assets/js/todo.js
@@ -0,0 +1,23 @@
+$("ul").on("click", "li", function markAsChecked() {
+    $(this).toggleClass("completed");
+})
+
+//click on x to delete todo
+$("ul").on("click", "span", function deleteTodo(event) {
+    //fades the li for 500 miliseconds and then removes if completely
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation(); //stops bubbling up to other elements
+
+});
+
+$('input[type="text"]').keypress(function(event) {
+    if (event.which === 13) {
+        var todoStr = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span>" + todoStr + "</li>");
+        
+    }
+
+});
